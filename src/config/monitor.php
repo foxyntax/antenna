3e1466e083764f7a194598235a5515fbf0dbb1ed
Legend <milad1995honor@gmail.com>
2021-03-18 11:42:59 +0330

    Update configuration

--- src/config/monitor.php
+++ src/config/monitor.php
@@ -4,11 +4,49 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Package Config
+    | Log Config
     |--------------------------------------------------------------------------
-    |
+    | Description will be written after the package will released.
     |
     */
-    'mail-address' => 'foxyntax@gmail.com'
+
+    'client_log' => [
+        'enabled'   => false,
+        'mail'      => '',
+        'tell'      => '',
+        'name'      => '',
+        'lang'      => 'en', // or fa
+        'period'    => 1
+    ],
+
+    'dev_log' => [
+        'API_KEY'       => '',
+        'route'         => '/get-logs', // return /monitor/get-logs in routes
+        'organization'  => 'foxyntax',
+        'lang'          => 'en', // or fa
+
+        'mail'  => [
+            'enabled'   => true,
+            'address'   => 'foxyntax@gmail.com',
+            'period'    => 1, // based on day 
+        ],
+
+        'sms'   => [
+            'enabled'   => false,
+            'tell'      => '09361719209',
+            'period'    => 1,
+
+            'driver'   => [
+                'kavenegar' => [
+                    'enabled'   => true,
+                    'API_KEY'   => ''
+                ],
+                // 'others'    => [
+                //     'enabled'   => true,
+                //     'API_KEY'   => ''
+                // ]
+            ]
+        ]
+    ]
 
 ];
