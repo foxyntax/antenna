0da655717422de8f3c03ea0b18272d27031c72d9
Legend <milad1995honor@gmail.com>
2021-07-31 17:20:37 +0430

    add and update translated for lara localization

---
+++ src/app/lang/en/sms.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Persian English
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in mail services, please add your
+    | mother tongue in case you haven't got your language on this package.
+    |
+    */
+
+    'title'        => 'Report bugs',
+    'package-name' => 'Monitoring | Foxyntax',
+    'bugs'         => ':log-count bugs found',
+    
+    'dev-title'    => ':app-name found new bugs, please fix it as soon as possible.',
+    'cli-title'    => ':app-name found new bugs, please contact your developer.',
+    
+    'dev-tell'     => 'Developer\'s Tell: :developer',
+    'cli-tell'     => 'Employer\'s Tell: :employer',
+    
+    'dev-mail'     => 'Developer\'s E-mail: :developer',
+    'cli-mail'     => 'Employer\'s E-mail: :employer'
+];
---
+++ src/app/lang/fa/sms.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Persian [Farsi] texts
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in mail services, please add your
+    | mother tongue in case you haven't got your language on this package.
+    |
+    */
+
+    'title'        => 'گزارش اشکال در برنامه',
+    'package-name' => 'مانیتورینگ | گروه فاکس سینتکس',
+    'bugs'         => ':log-count عدد باگ پیدا شد',
+    
+    'dev-title'    => 'وب سایت :app-name اشکالات جدید را پیدا کرده است، لطفا در اسرع وقت آن‌ها را اصلاح کنید.',
+    'cli-title'    => 'وب سایت :app-name اشکالات جدید را پیدا کرده است، لطفا با سازنده وب سایت تماس بگیرید.',
+    
+    'dev-tell'     => 'شماره تماس سازنده: :developer',
+    'cli-tell'     => 'شماره تماس کارفرما :employer',
+    
+    'dev-mail'     => 'پست الکترونیک سازنده :developer',
+    'cli-mail'     => 'پست الکترونیک کارفرما :employer'
+];
