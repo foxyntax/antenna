0e9ab50c27c72b9ee9dbd6f478f37477b145b569
Legend <milad1995honor@gmail.com>
2021-07-31 12:48:02 +0430

    add en localization

---
+++ src/app/lang/en/mail.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | English texts
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in mail services, please add your
+    | mother tongue in case you haven't got your language on this package.
+    |
+    */
+
+    'meta-title'        => 'Report bugs',
+    'package-name'      => 'Monitoring | Foxyntax',
+    'mail-title'        => ':log-count bugs found',
+    'dev-desc-title'    => ':app-name found new bugs, please fix it as soon as possible.',
+    'cli-desc-title'    => ':app-name found new bugs, please contact your developer.',
+    'report-time'       => 'Reported at :time',
+    'dev-warning-note'  => 'This report(s) is made for developers, if you\'re not a developer or contributer, do not change anything in your app codes.',
+    'cli-warning-note'  => 'The report is not contain bugs, this app may sent the copy of them to your programmer, too.',
+    'info-title'        => ':person information:',
+    'name'              => 'Employer\'s fullname:',
+    'location'          => 'Location:',
+    'tell'              => 'Tell:',
+    'default-log-title' => 'The #:key',
+    'host-action'       => 'Go to host',
+    'call-action'       => 'Call your developer',
+    'happy-to-use'      => 'Are you happy to use foxyntax monitoring package??',
+    'happy-to-use'      => 'Are you happy to use foxyntax monitoring package??',
+    'donate-part1'      => 'donate me',
+    'donate-part2'      => 'to add more features in future package upgrades',
+    'copyright'         => 'Created by Milad Mohammadi | Foxyntax',
+];
