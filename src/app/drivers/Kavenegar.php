5ca850e5e33754357f42c2d41a10f85a0dd88f6d
Legend <milad1995honor@gmail.com>
2021-08-10 22:16:14 +0430

    add kavenegar driver

---
+++ src/app/drivers/Kavenegar.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Foxyntax\Monitoring\App\Drivers;
+
+use Foxyntax\Monitoring\App\Interfaces\DriverRegisteration;
+use Kavenegar\KavenegarApi;
+use Kavenegar\Exceptions\ApiException;
+use Kavenegar\Exceptions\HttpException;
+
+class Kavenegar implements DriverRegisteration {
+
+
+    /**
+     ** Kavenegar API instance
+     * 
+     * @var \Kavenegar\KavenegarApi
+     */
+    protected $api;
+
+
+    /**
+     ** Kavenegar sender
+     * 
+     * @var $sender
+     */
+    protected $sender;
+
+
+
+    /**
+     ** Register the SMS package that you want to use it on monitoring package
+     *
+     * @return null 
+     */
+    public function register()
+    {
+        $config = config('monitor.sms.drivers');
+        $this->api = new KavenegarApi($config['kavenegar']['API_KEY']);
+        $this->sender = $config['kavenegar']['sender'];
+    }
+
+    /**
+     ** Send a SMS based
+     * 
+     * @param array $config
+     * @param string $message
+     */
+    public function send(array $config, string $message)
+    {
+        try {
+            $this->register();
+            $this->api->send($config['tell'], $message, $this->sender);
+        }
+        catch(ApiException $e){
+            return response()->json($e->errorMessage(), 412);
+        }
+        catch(HttpException $e){
+            return response()->json($e->errorMessage(), 412);
+        }
+    }
+}
