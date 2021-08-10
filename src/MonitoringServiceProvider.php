e430f21e50c5ffe831ee701717f0c3d30562aa9b
Legend <milad1995honor@gmail.com>
2021-08-10 22:15:20 +0430

    add new load and publish funcs

--- src/MonitoringServiceProvider.php
+++ src/MonitoringServiceProvider.php
@@ -13,7 +13,9 @@ class MonitoringServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->mergeConfigFrom(
+            __DIR__.'/config/monitor.php', 'monitor'
+        );
     }
 
     /**
@@ -23,8 +25,25 @@ class MonitoringServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Publish configuration file
         $this->publishes([
             __DIR__.'/config/monitor.php' => config_path('monitor.php'),
-        ]);
+        ], 'config');
+
+        // Publish views [email views] file
+        $this->publishes([
+            __DIR__.'/views/development.php'=> resource_path('views/emails/fx_development.blade.php'),
+            __DIR__.'/views/client.php'     => resource_path('views/emails/fx_client.blade.php')
+        ], 'views');
+
+        // Publishing translations
+        $this->loadTranslationsFrom(__DIR__.'/app/lang', 'monitoring');
+
+        // Load migration
+        $this->loadMigrationsFrom(__DIR__.'/migrations');
+
+        // Load API routes
+        $this->loadRoutesFrom(__DIR__.'/routes/api.php');
+        
     }
 }
